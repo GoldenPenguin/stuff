9a0035d66bc0c39d507a4d66b97cabc80d2c86db
GoldenPenguin <mathieu.b.belanger@usherbrooke.ca>
2019-04-25 14:42:29 -0400

    Create init.el

---
+++ emacs/init.el
@@ -0,0 +1,16 @@
+;;; Set Melpa as an additionnal package repository
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+;;; Bootstrapping use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Other generated configuration ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
